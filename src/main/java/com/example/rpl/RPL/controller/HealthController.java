0da285e90d580de64d345fe8a65d6cc2ea0574ee
alejandro <levinasale@gmail.com>
2019-09-18 21:52:43 -0300

    Modified HealthController to add logging

--- src/main/java/com/example/rpl/RPL/controller/HealthController.java
+++ src/main/java/com/example/rpl/RPL/controller/HealthController.java
@@ -1,15 +1,18 @@
 package com.example.rpl.RPL.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthController {
 
     @GetMapping(value = "/api/health", produces = "plain/text")
     public ResponseEntity<String> ping() {
+        log.info("PING");
         return new ResponseEntity<>("pong", HttpStatus.OK);
     }
 }
