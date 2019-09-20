23462df0b181465d7e255c01d41f7954015b200a
alejandro <levinasale@gmail.com>
2019-09-20 14:38:28 -0300

    Added 'password' field to User and encoded it during signup

--- src/main/java/com/example/rpl/RPL/repository/UserRepository.java
+++ src/main/java/com/example/rpl/RPL/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.example.rpl.RPL.repository;
 
 import com.example.rpl.RPL.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
 
-    User findUserByUsername(String username);
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
