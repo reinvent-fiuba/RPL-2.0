408c1f7e684165859c5e33e1cab5cce354dcc308
alejandro <levinasale@gmail.com>
2019-09-24 13:29:08 -0300

    Returning 400 instead of 422 on validation errors

--- src/test/groovy/com/example/rpl/RPL/controller/HealthControllerFunctionalSpec.groovy
+++ src/test/groovy/com/example/rpl/RPL/controller/HealthControllerFunctionalSpec.groovy
@@ -11,9 +11,10 @@ class HealthControllerFunctionalSpec extends AbstractFunctionalSpec {
 
     def "Ping must return pong"() {
         when: "get ping url"
-            def response = get("/api/health")
+            def response = get("/health")
 
         then: "must return a plain text containing pong"
+            response.contentType == "plain/text;charset=UTF-8"
             response.statusCode == SC_OK
             response.body.asString() == "pong"
     }
