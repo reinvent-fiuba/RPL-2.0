17d1fed70442af1479a3748728f46d7b880da686
alejandro <levinasale@gmail.com>
2020-06-17 19:53:00 -0300

    Updated initial files

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -71,7 +71,7 @@ VALUES (4, 1, 4, 2, false, now(), now());
 
 INSERT INTO rpl_files
 VALUES (1, 'activity_1_starting_filess.tar.gz', 'text/x-c',
-        x'1f8b0800af122e5e0003edd2bf0ac23010c7f13ccaad2e92d87fe0ee83847a60a5b63569c4c737bae8224e45c4ef67f941ee86cb7141fdfea4ebf93a9bc5d8acaecb7bbaa6b2aff9e09c35aea85d51daa6292a63dda6dc382376b9919e529c7d1031bed769bcbeeffb54ff51bb38ab689434880feda1bb8c926f41ce49a51fa3e49736c79474af83a4985735f9e025a6287ad4d0766d97ebdbd5b7ff0100000000000000000000000000ffe606912b5d9c00280000',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
         now(), now());
 
 INSERT INTO rpl_files
@@ -104,6 +104,45 @@ VALUES (7, 'multi_submission_c_unit_test.tar.xz', 'text/x-c',
         x'fd377a585a000004e6d6b4460200210116000000742fe5a3e011ff01ce5d0036984972850270e93a4742c2b005c0f9915c9e4405b5de90fd4154b1a141aa4f3e98954290b8b3da7d29a45adda34ebac0f541e09a1b6987d8026136bb331c91f18794cf969b3a641380074914bd2bc492bf571c6a171d59703945ca0a005e7bb9a4d9876b22b092deb1ee65ded485ae0ab7291bd333495f58c37e3e28f78832c8ae017a24de721524a06bf737b77e65d9b4164020158fd78fd643c46ace8465b4fd390fa08907fe507dfc6b3998939fcf087c8cded1770e7d6d4c56c897f0a12910a9a2732312f1c6c7034001372dc40b6b9c1b2c1db101f8c42241620b0af437dc48a8ca064ecce2885395d7a601df277aa23068c13fcac508ef61aa2152e851e71ff2d86eadebc90077a2bdee8280a435302e17135e44e944acc91aeb012f7ce121b926f5668da04353ac020346ce9234616c675185d12b34192b9f479195388d210f655d7fc0b5d573e8f53d17f20f580c9a6ac73f174d31e2fcffef02419d4b1132284253f5e020d00074299ef0f785b8bc08d2b51c1341c23545e2518ca8ce172db1d8ad54f7f3f3cf93a9583ec250a1cbd436240a6609760811d975c214a4a8d094d0bf890e13ded67ab4b27960222cf67fd86bbe18111623b1b07188607126d7c85fa47baafb47ae762a000000a655d15ce5c5f5030001ea0380240000eece37e1b1c467fb020000000004595a',
         now(), now());
 
+INSERT INTO rpl_files
+VALUES (8, 'activity_2_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
+        now(), now());
+INSERT INTO rpl_files
+VALUES (9, 'activity_3_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800069dea5e0003edd4eb6a83301407703ffb1407cbc04269a3550b0ef7240309d3b6428d62226c8cbefb9254bbee46b7b12bfc7f947ac9c9495a3d874b596d445d0a95d7bc12f3f6cef9724c4b92c81c8355cc4e8f561c31275826ab300ea238081d1644411c3bc4be7e2b2ff552f18ec8e1bbb26d6edf8e3b37fe4f5575db748a5455ea13d7758b724de645f0a7a94bc3ed5c969b5e148da48c2aa1fc4ab4bdf2a75357476ce58cea4acc48c7e8e1c384f93821e7f9b6e9b8cc7548aff4f538e03fcb6c734da8ed747ef2bd0b1616e9f0b59a47eb6be1cdf44a66213d637a69a26dacaf4f88bc7bbb4acaaec27d7a3fac355c8d2ba436d1de9baf9baee6cab733b261fb263e33d96dbacf1897c9ccfef4af710f7fa2fbdbcff79ce1817d47d91f9dabffe52a1eeb3fd61f5dffe13260a8ff9f60eafde3d56a7ac3e4a629aa4d4345b923beeb6bd198a2e452da4adeca6337786c1f8b052d13c66cc0cbd6f2fccec563b0ded0ebe992c3f869f3394d7018b6215da9fa4e3ce9577fbe3801000000000000000000000000000000dee101fd71fd3600280000',
+        now(), now());
+INSERT INTO rpl_files
+VALUES (10, 'activity_4_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800069dea5e0003edd4eb6a83301407703ffb1407cbc04269a3550b0ef7240309d3b6428d62226c8cbefb9254bbee46b7b12bfc7f947ac9c9495a3d874b596d445d0a95d7bc12f3f6cef9724c4b92c81c8355cc4e8f561c31275826ab300ea238081d1644411c3bc4be7e2b2ff552f18ec8e1bbb26d6edf8e3b37fe4f5575db748a5455ea13d7758b724de645f0a7a94bc3ed5c969b5e148da48c2aa1fc4ab4bdf2a75357476ce58cea4acc48c7e8e1c384f93821e7f9b6e9b8cc7548aff4f538e03fcb6c734da8ed747ef2bd0b1616e9f0b59a47eb6be1cdf44a66213d637a69a26dacaf4f88bc7bbb4acaaec27d7a3fac355c8d2ba436d1de9baf9baee6cab733b261fb263e33d96dbacf1897c9ccfef4af710f7fa2fbdbcff79ce1817d47d91f9dabffe52a1eeb3fd61f5dffe13260a8ff9f60eafde3d56a7ac3e4a629aa4d4345b923beeb6bd198a2e452da4adeca6337786c1f8b052d13c66cc0cbd6f2fccec563b0ded0ebe992c3f869f3394d7018b6215da9fa4e3ce9577fbe3801000000000000000000000000000000dee101fd71fd3600280000',
+        now(), now());
+INSERT INTO rpl_files
+VALUES (11, 'activity_5_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
+        now(), now());
+INSERT INTO rpl_files
+VALUES (12, 'activity_6_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
+        now(), now());
+INSERT INTO rpl_files
+VALUES (13, 'activity_7_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
+        now(), now());
+INSERT INTO rpl_files
+VALUES (14, 'activity_8_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
+        now(), now());
+
+INSERT INTO rpl_files
+VALUES (15, 'activity_8_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
+        now(), now());
+
+INSERT INTO rpl_files
+VALUES (16, 'activity_8_starting_filess.tar.gz', 'text/x-c',
+        x'1f8b0800d89dea5e0003edd34f6b8330180670cff914c1b1d21691c4f907e6d8a5f4b07337d8a12059b555b0b11885c1d8775f4c758cd2d1cbbab2f1fc0eb14dde3751f4d98a42ba2bebac9816867e77e551c0be5e0d3f882c7e13465ec0fd807b16e33cf0428bb2f3ded65eab1a51536a8932db55afdfd79d5affa3ae0ab92adb34a377aa49cbe2c5cdefc9e79cdd14d97657b9b94d08296443b7fa6b194fe81ba15a37b12f480ab9a93325d22a266649ad845cd3b17d9ddacee8b066d217990d0be9d05cc566625d56a2a12adb0cbb649b56a6954a449257b55089ae6e1bfd7f58181f6eedd051aef460b61de9b2e1ac5dad4f3377c4bcf4b61f22d75f2fa5ede896ae615fde5577439d356d2de9fcf9e131593ccd66f3c52226ef977e5d3f6e78c3e73ce354fe79e41fe4dfe38c21ffbfe158fe4dd44f87ef58fe1d93eaa9e9e87ff77d4e1fefe9d03f892ffdec00000000000000000000000000000000ffc107f20e928f00280000',
+        now(), now());
+
 INSERT INTO activity_categories
 VALUES (1, 1, 'Conceptos Basicos',
         'Ejercicios faciles para empezar', true, now(), now());
@@ -116,23 +155,26 @@ VALUES (1, 1, 1, 'Pasar de segundos a horas, minutos, segundos IO Test',
 
 INSERT INTO activities
 VALUES (2, 1, 1, 'Ej2 Unit Test C',
-        'La descripción del ejericio 2', 'c_std11', false, true, false, '//El codigo inicial', 1, 10,
+        'La descripción del ejericio 2', 'c_std11', false, true, false, '//El codigo inicial', 8,
+        10,
         now(), now());
 
 INSERT INTO activities
 VALUES (3, 1, 1, 'Ej 3 Unit Test Python',
-        'La descripción del ejericio 3', 'python_3.7', false, true, false, '#El codigo inicial', 1, 10,
+        'La descripción del ejericio 3', 'python_3.7', false, true, false, '#El codigo inicial', 9,
+        10,
         now(), now());
 
 INSERT INTO activities
 VALUES (4, 1, 1, 'Ej 4 IO Test Python',
-        'La descripción del ejericio 4', 'python_3.7', true, true, false, '#El codigo inicial', 1,
+        'La descripción del ejericio 4', 'python_3.7', true, true, false, '#El codigo inicial', 10,
         10,
         now(), now());
 
 INSERT INTO activities
 VALUES (5, 1, 1, 'El ejericio 5',
-        'La descripción del ejericio 5', 'c_std11', true, true, false, '//El codigo inicial', 1, 10,
+        'La descripción del ejericio 5', 'c_std11', true, true, false, '//El codigo inicial', 11,
+        10,
         now(), now());
 
 INSERT INTO activity_categories
@@ -143,27 +185,31 @@ VALUES (2, 1, 'Conceptos no tan Basicos',
 INSERT INTO activities
 VALUES (6, 1, 2, 'Pasar de segundos a horas, minutos, segundos',
         'Dada una cantidad de segundos, devolver/imprimir la cantidad de horas', 'c_std11', true,
-        true, false, '//El codigo inicial', 1, 10,
+        true, false, '//El codigo inicial', 12, 10,
         now(), now());
 
 INSERT INTO activities
 VALUES (7, 1, 2, 'El ejericio 2',
-        'La descripción del ejericio 2', 'c_std11', true, true, false, '//El codigo inicial', 1, 10,
+        'La descripción del ejericio 2', 'c_std11', true, true, false, '//El codigo inicial', 13,
+        10,
         now(), now());
 
 INSERT INTO activities
 VALUES (8, 1, 2, 'El ejericio 3',
-        'La descripción del ejericio 3', 'c_std11', true, true, false, '//El codigo inicial', 1, 10,
+        'La descripción del ejericio 3', 'c_std11', true, true, false, '//El codigo inicial', 14,
+        10,
         now(), now());
 
 INSERT INTO activities
 VALUES (9, 1, 2, 'El ejericio 4',
-        'La descripción del ejericio 4', 'c_std11', true, true, false, '//El codigo inicial', 1, 10,
+        'La descripción del ejericio 4', 'c_std11', true, true, false, '//El codigo inicial', 15,
+        10,
         now(), now());
 
 INSERT INTO activities
 VALUES (10, 1, 2, 'El ejericio 5',
-        'La descripción del ejericio 5', 'c_std11', true, true, false, '//El codigo inicial', 1, 10,
+        'La descripción del ejericio 5', 'c_std11', true, true, false, '//El codigo inicial', 16,
+        10,
         now(), now());
 
 INSERT INTO activity_submissions
--- src/main/resources/db/testdata/multi_submission_python/tiempo.py
+++ src/main/resources/db/testdata/multi_submission_python/tiempo.py
@@ -1,7 +1,9 @@
 def segundos_a_horas_minutos_segundos(tiempo_segundos):
-  hs = tiempo_segundos // 3600
-  tiempo_segundos = tiempo_segundos % 3600
-  min = tiempo_segundos // 60
-  seg = tiempo_segundos % 60
-
-  return hs, min, seg
+  # codigo del alumno
+  pass
+  # hs = tiempo_segundos // 3600
+  # tiempo_segundos = tiempo_segundos % 3600
+  # min = tiempo_segundos // 60
+  # seg = tiempo_segundos % 60
+  #
+  # return hs, min, seg
