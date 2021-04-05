1b70835a8893beb42d109988af86ff6285a03457
tutecano1995 <tutecano1995@gmail.com>
2021-04-05 12:14:00 -0300

    Review suggestions

--- kubernetes/mysql/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x #echo on
-
-mysql_pod=$1;
-
-kubectl exec -it "$mysql_pod" -- mysqldump --all-databases --hex-blob -uroot -pBh2gujvNJOD7e9sa > all-databases.sql;
-sed -i 1d all-databases.sql;
-tar -zcvf all-databases.tar.gz all-databases.sql;
